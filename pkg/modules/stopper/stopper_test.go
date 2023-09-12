1af2283fe5c3754dc0aa956c1bc62808c93f80ef
Lavysh Alexander <LavyshAlexander@gmail.com>
2023-09-12 11:15:16 +0300

    Added unit test for stopper from another module.

--- pkg/modules/stopper/stopper_test.go
+++ pkg/modules/stopper/stopper_test.go
@@ -2,11 +2,12 @@ package stopper
 
 import (
 	"context"
+	"github.com/dipdup-net/indexer-sdk/pkg/modules"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestStopperCallsStop(t *testing.T) {
+func TestStopper_CallsStop(t *testing.T) {
 	stopWasCalled := false
 
 	var stopFunc context.CancelFunc = func() {
@@ -24,6 +25,38 @@ func TestStopperCallsStop(t *testing.T) {
 
 	// Act: send stop signal to stopper
 	stopperInput.Push(struct{}{})
+
+	err = stopperModule.Close()
+	assert.NoError(t, err)
+
+	assert.True(t, stopWasCalled, "stop was never called")
+}
+
+func TestStopper_CallsStopFromAnotherModule(t *testing.T) {
+	stopWasCalled := false
+
+	var stopFunc context.CancelFunc = func() {
+		stopWasCalled = true
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	stopperModule := NewModule(stopFunc)
+	workerModule := &modules.BaseModule{}
+	workerModule.Init("worker")
+	workerModule.CreateOutput("stop")
+
+	err := stopperModule.AttachTo(workerModule, "stop", InputName)
+	assert.NoError(t, err)
+
+	stopperModule.Start(ctx)
+
+	workerOutput, err := workerModule.Output("stop")
+	assert.NoError(t, err)
+
+	// Act: send stop signal to stopper
+	workerOutput.Push(struct{}{})
 
 	err = stopperModule.Close()
 	assert.NoError(t, err)
